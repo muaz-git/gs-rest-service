d4408f87f20ef66758524f0bffe0cba81a11038a
Muaz Usmani <muhammad.muaz062@gmail.com>
2019-08-13 14:03:57 +0200

    Completed Server.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,6 +2,12 @@ package hello;
 
 import hello.storage.StorageProperties;
 import hello.storage.StorageService;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.util.ModelBuilder;
+import org.eclipse.rdf4j.model.vocabulary.FOAF;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,14 +15,45 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
         SpringApplication.run(Application.class, args);
+//        Application.dummyXML();
     }
 
+    private static void dummyXML() throws FileNotFoundException {
+        ModelBuilder builder = new ModelBuilder();
+        builder.setNamespace("ex", "http://example.org/");
+
+        // in named graph 1, we add info about Picasso
+        builder.namedGraph("ex:namedGraph1")
+                .subject("ex:Picasso")
+                .add(RDF.TYPE, "artist")
+                .add(FOAF.FIRST_NAME, "Pablo");
+
+        // in named graph2, we add info about Van Gogh.
+        builder.namedGraph("ex:namedGraph2")
+                .subject("ex:VanGogh")
+                .add(RDF.TYPE, "artist")
+                .add(FOAF.FIRST_NAME, "Vincent");
+
+
+        // We're done building, create our Model
+        Model model = builder.build();
+
+        File file2 = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\" + "resources.rdf");
+
+        FileOutputStream fop = new FileOutputStream(file2);
+        Rio.write(model, fop, RDFFormat.RDFXML);
+    }
     @Bean
     CommandLineRunner init(StorageService storageService) {
         return (args) -> {
--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -6,9 +6,6 @@ import java.util.concurrent.atomic.AtomicLong;
 
 
 import hello.storage.StorageService;
-import org.eclipse.rdf4j.model.*;
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
-import org.eclipse.rdf4j.model.impl.TreeModel;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,14 +15,10 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
-import org.eclipse.rdf4j.model.vocabulary.FOAF;
-import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.Rio;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.rio.RDFFormat;
-import org.eclipse.rdf4j.rio.Rio;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -38,6 +31,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     private final StorageService storageService;
+    private File repo = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\" + "resources.rdf");
 
     @Autowired
     public GreetingController(StorageService storageService) {
@@ -48,141 +42,47 @@ public class GreetingController {
     @PostMapping("/greeting")
     public Greeting handleFileUpload(@RequestParam("file") MultipartFile file,
                                      RedirectAttributes redirectAttributes) throws IOException {
-        this.getNamedGraph();
-        storageService.store(file);
-//        this.testing(file);
-//        this.ex(file);
-//        redirectAttributes.addFlashAttribute("message",
-//                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        storageService.store(file); // saves the incoming file
+        Model curr_model = this.getModelCurrentFile(file); // loads RDF model of incoming file in to java object.
+        this.updateRepo(curr_model); // Update the database with current object with the previous ones.
 
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, "name"));
     }
 
-    void getNamedGraph() throws IOException {
-//        FileOutputStream fop = null;
-        File file2;
-//        // We'll use a ModelBuilder to create two named graphs, one containing data about
-//        // picasso, the other about Van Gogh.
-//        ModelBuilder builder = new ModelBuilder();
-//        builder.setNamespace("ex", "http://example.org/");
-//
-//        // in named graph 1, we add info about Picasso
-//        builder.namedGraph("ex:namedGraph1")
-//                .subject("ex:Picasso")
-//                .add(RDF.TYPE, "artist")
-//                .add(FOAF.FIRST_NAME, "Pablo");
-//
-//        // in named graph2, we add info about Van Gogh.
-//        builder.namedGraph("ex:namedGraph2")
-//                .subject("ex:VanGogh")
-//                .add(RDF.TYPE, "artist")
-//                .add(FOAF.FIRST_NAME, "Vincent");
-//
-//
-//        // We're done building, create our Model
-//        Model model = builder.build();
-        file2 = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\" +"resources.rdf");
-//        fop = new FileOutputStream(file2);
-//
-//        Rio.write(model, fop, RDFFormat.RDFXML);
-
-        InputStream input =new FileInputStream(file2);
-
-        Model model = Rio.parse(input, "", RDFFormat.RDFXML);
-        for (Statement statement: model) {
-            System.out.println(statement);
-        }
-    }
-
-    void ex(MultipartFile file) throws IOException {
-        String fpath = System.getProperty("user.dir") + "\\upload-dir\\" + file.getOriginalFilename();
-        // read the file 'example-data-artists.ttl' as an InputStream.
-        InputStream input =new FileInputStream(fpath);
-
-//                GreetingController.class.getResourceAsStream("/" + fpath);
-
-        // Rio also accepts a java.io.Reader as input for the parser.
-        try {
-            Model model = Rio.parse(input, "", RDFFormat.RDFXML);
-        }
-
-        // To check that we have correctly read the file, let's print out the model to the screen again
-        for (Statement statement: model) {
-            System.out.println(statement);
-        }
-
-    }
-
-    void testing(MultipartFile file) throws IOException {
-        String fpath = System.getProperty("user.dir") + "\\upload-dir\\" + file.getOriginalFilename();
-        FileOutputStream fop = null;
-        File file2;
-        // We'll use a ModelBuilder to create two named graphs, one containing data about
-        // picasso, the other about Van Gogh.
+    private void updateRepo(Model currModel) throws IOException {
         ModelBuilder builder = new ModelBuilder();
         builder.setNamespace("ex", "http://example.org/");
 
-        // in named graph 1, we add info about Picasso
-        builder.namedGraph("ex:namedGraph1")
-                .subject("ex:Picasso")
-                .add(RDF.TYPE, "artist")
-                .add(FOAF.FIRST_NAME, "Pablo");
+        for (Statement statement : currModel) {
+            builder.namedGraph("ex:namedGraph1")
+                    .subject(statement.getSubject())
+                    .add(statement.getPredicate(), statement.getObject());
+        }
 
-        // in named graph2, we add info about Van Gogh.
-        builder.namedGraph("ex:namedGraph2")
-                .subject("ex:VanGogh")
-                .add(RDF.TYPE, "artist")
-                .add(FOAF.FIRST_NAME, "Vincent");
+        if (this.repo.exists()) {
+            // if resource exist, just append new results to them.
 
+            InputStream input = new FileInputStream(this.repo);
+            Model rootModel = Rio.parse(input, "", RDFFormat.RDFXML);
 
-        // We're done building, create our Model
+            for (Statement statement : rootModel) {
+                builder.namedGraph("ex:namedGraph1")
+                        .subject(statement.getSubject())
+                        .add(statement.getPredicate(), statement.getObject());
+            }
+        }
+
+        FileOutputStream fop = new FileOutputStream(this.repo);
         Model model = builder.build();
-        file2 = new File(System.getProperty("user.dir") + "\\upload-dir\\" +"newfile.txt");
-        fop = new FileOutputStream(file2);
-
         Rio.write(model, fop, RDFFormat.RDFXML);
-
-//        try {
-//
-//            file2 = new File(System.getProperty("user.dir") + "\\upload-dir\\" +"newfile.txt");
-//            fop = new FileOutputStream(file2);
-//
-//            // each named graph is stored as a separate context in our Model
-//            for (Resource context : model.contexts()) {
-//                System.out.println("Named graph " + context + " contains: ");
-//
-//                // write _only_ the statemements in the current named graph to the console, in N-Triples format
-//                Rio.write(model.filter(null, null, null, context), fop, RDFFormat.NTRIPLES);
-//                System.out.println();
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if (fop != null) {
-//                    fop.close();
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-
-
-
     }
 
-
-    void printer(MultipartFile file) throws IOException {
-//        String fpath = System.getProperty("user.dir") + "\\upload-dir\\" + file.getOriginalFilename();
-//        System.out.println("Present Project Directory : " + fpath);
-//
-//        FileInputStream fin = new FileInputStream(fpath);
-//        Model model = Rio.parse(fin, "", RDFFormat.RDFXML);
-//        ValueFactory vf = SimpleValueFactory.getInstance();
-
-
+    private Model getModelCurrentFile(MultipartFile file) throws IOException {
+        String fpath = System.getProperty("user.dir") + "\\upload-dir\\" + file.getOriginalFilename();
+        InputStream input = new FileInputStream(fpath);
+        return Rio.parse(input, "", RDFFormat.RDFXML);
     }
 
 }
